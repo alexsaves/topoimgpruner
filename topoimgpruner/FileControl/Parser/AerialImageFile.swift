df372370ab23e18b9a5de40f523138da4df7eb05
alexsaves <alexei.white@gmail.com>
2023-03-01 06:29:22 -0800

    Crawling works. Moving on to image parsing.

---
+++ topoimgpruner/FileControl/Parser/AerialImageFile.swift
@@ -0,0 +1,12 @@
+//
+//  AerialImageFile.swift
+//  topoimgpruner
+//
+//  Created by Alexei White on 3/1/23.
+//
+
+import Foundation
+
+class AerialImageFile {
+    
+}
--- topoimgpruner/FileControl/Parser/DirParser.swift
+++ topoimgpruner/FileControl/Parser/DirParser.swift
@@ -16,36 +16,67 @@ class DirParser {
     public var rootFolder: String = ""
     
     /**
-    * Set up a new instance of the parser
-    */
+     * Set up a new instance of the parser
+     */
     init(root: String) {
         self.rootFolder = root
     }
     
     /**
+     * Get the list of possible files first
+     */
+    private func crawlForFiles() -> [String] {
+        var _filesToCheck: [String] = []
+        let fm = FileManager.default
+        //print("Scanning folder \(rootFolder)...")
+        do {
+            let url = URL(fileURLWithPath: rootFolder)
+            if let enumerator = fm.enumerator(at: url, includingPropertiesForKeys: [.isRegularFileKey], options: [.skipsHiddenFiles, .skipsPackageDescendants]) {
+                for case let fileURL as URL in enumerator {
+                    let fileAttributes = try fileURL.resourceValues(forKeys:[.isRegularFileKey])
+                    if fileAttributes.isRegularFile! {
+                        //print("Looking at regular file \(fileURL.path).")
+                        if !_filesToCheck.contains(fileURL.path) {
+                            _filesToCheck.append(fileURL.path)
+                        }
+                    }
+                }
+            }
+            
+        } catch {
+            // Error on file IO
+            events.trigger(eventName: "error", information: "IO Error on parser")
+        }
+        return _filesToCheck
+    }
+    
+    /**
+     * Produce a finished list of jpegs
+     */
+    private func filterValidAerialJPEGS(filesToCheck: [String]) -> [AerialImageFile] {
+        var _finalImageSet: [AerialImageFile] = []
+        
+        return _finalImageSet
+    }
+    
+    /**
     * Start a parse operation
      */
-    func parse() {
-        var _foldersToCrawl: [String] = []
-        _foldersToCrawl.append(rootFolder)
-        
-        
-            // Iterate over the folders
-            while _foldersToCrawl.count > 0 {
-                let _folder: String = _foldersToCrawl.popLast()!
-                let fm = FileManager.default
-                do {
-                    let items = try fm.contentsOfDirectory(atPath: _folder)
-                    for item in items {
-                        print("Found \(item)")
-                    }
-                } catch {
-                    // Error on file IO
-                    events.trigger(eventName: "error", information: "IO Error on parser")
-                    break;
-                }
-                
+    func parse() -> [AerialImageFile] {
+        var _filesToCheck: [String] = crawlForFiles()
+        if _filesToCheck.count == 0 {
+            // Error on scan
+            events.trigger(eventName: "error", information: "Found no eligible files to scan")
+            return []
+        } else {
+            var _finalImageSet: [AerialImageFile] = filterValidAerialJPEGS(filesToCheck: _filesToCheck)
+            if _filesToCheck.count == 0 {
+                // Error on scan
+                events.trigger(eventName: "error", information: "Found no eligible aerial images from scan")
+                return []
+            } else {
+                return _finalImageSet
             }
-        
+        }
     }
 }
